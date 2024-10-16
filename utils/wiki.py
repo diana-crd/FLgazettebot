58a0e2989948e6861af434c670f58780602fcae2
diana-laptop <cervidiana327@gmail.com>
2024-10-16 15:29:06 +0200

    improved wiki functionality, added item info

--- utils/wiki.py
+++ utils/wiki.py
@@ -1,12 +1,11 @@
 import requests
 import json
-from re import findall, search, finditer
+from re import findall, search
 from os.path import isfile
 from time import sleep
 
 
 api_endpoint = "https://fallenlondon.wiki/w/api.php"
-wiki_url = "https://fallenlondon.wiki/wiki/"
 
 headers = {
     'User-Agent': 'FLGazetteBot/0.1 python requests',
@@ -16,6 +15,10 @@ headers = {
 rgx_string = (r'<section begin=\\"Current Value\\"/>(\d+)<section end=\\"Current Value\\"/>\|'
               r'<section begin=\\"Last Updated\\"/>(\d+)<section end=\\"Last Updated\\"/>')
 qvalue_string = r"Value(\d+) = ([ \-\w]+)\\n"
+equip_string = r"Effects(\d+) = {{IL\|([ \-\w]+)}} ([\-\+\d]+)"
+item_type_string = r"Item Type = ([ \-\w]+)"
+alt_item_type_string = r"{{(\w+)"
+redirect_string = r"\[\[([ \-\w]+)\]\]"
 
 with open("info/pagetitle.json") as f:
     wikipages: dict[str, str] = json.load(f)
@@ -31,8 +34,14 @@ def get_wikitext(page):
         "page": page
     }
     wiki_req = requests.get(url=api_endpoint, params=params, headers=headers)
+    wiki_dict = wiki_req.json()
+    if "error" in wiki_dict.keys():
+        return None
     #TODO: catch bad requests
-    wiki_text = wiki_req.content.decode()
+    wiki_text = wiki_dict["parse"]["wikitext"]["*"]
+    if "REDIRECT" in wiki_text:
+        tru_page = search(redirect_string, wiki_text)
+        wiki_text = get_wikitext(tru_page)
 
     return wiki_text
 
@@ -57,21 +66,83 @@ def parse_quality_values():
     with open('info/raw_wiki.json', 'w', encoding='utf-8') as f:
         json.dump(qvalues, f, ensure_ascii=False, indent=4)
 
+def parse_item_stats(item):
+    print(item)
+    wikitxt = get_wikitext(item)
+    stats = {}
+    if wikitxt is None:
+        return "custom", stats
+    
+    
+    effects_list = findall(equip_string, wikitxt) # [("effectN", "stat", "stat_value")...]
+    if not effects_list:
+        try:
+            item_type = search(alt_item_type_string, wikitxt).group(1)
+        except:
+            print(wikitxt)
+    
+    else:
+        item_type = search(item_type_string, wikitxt).group(1)
+        for effects in effects_list:
+            stats[effects[1]] = effects[2]
+
+    return item_type, stats
+
+def parse_all_stats():
+    with open("info/items.json") as f:
+        items_dict = json.load(f)
+    
+    for item, item_dict in items_dict.items():
+        if "type" in item_dict.keys():
+            continue
+        # if item_dict["type"] in ("Item", "Quality"):
+        #     continue
+        item_type, stats = parse_item_stats(item)
+        item_dict["type"] = item_type
+        item_dict["stats"] = stats
+        
+        if stats:
+            perk_str = "grants "
+            for stat, stat_value in stats.items():
+                perk_str += f"{stat_value} {stat}, "
+            item_dict["perk"] = perk_str[:-2]
+    
+    with open("info/items.json", 'w', encoding='utf-8') as f:
+        json.dump(items_dict, f, ensure_ascii=False, indent=4)
+            
+    
+
+def extract_dict_items(qdict):
+    item_list = []
+    if "item" in qdict.keys() and not "|" in qdict["item"]:
+        item_list.append(qdict["item"])
+    if "items" in qdict.keys():
+        item_list.extend(qdict["items"])
+
+    return item_list
+
 def dump_items():
     items_list = []
-    items_dict = {}
+    with open("info/items.json") as f:
+        items_dict = json.load(f)
+
+    default_dict = {"alias": "", "ratvalue": "", "kind" :"", "perk": ""}
+
     for quality, quality_dict in wikidict.items():
         if quality == "demand":
             continue
 
-        for value in quality_dict["values"].values():
-            if "items" in value.keys():
-                items_list.extend(value["items"])
+        items_list.extend(extract_dict_items(quality_dict))
+        if "values" in quality_dict.keys():
+            for value_dict in quality_dict["values"].values():
+                items_list.extend(extract_dict_items(value_dict))
+
     items_set = set(items_list)
-    for item in items_set:
-        items_dict[item] = {"alias": "", "ratvalue": "", "value": ""}
+    missing_items = [item for item in items_set if item not in items_dict.keys()]
+    for item in missing_items:
+        items_dict[item] = default_dict
     
-    with open("info/raw_items.json", 'w', encoding='utf-8') as f:
+    with open("info/items.json", 'w', encoding='utf-8') as f:
         json.dump(items_dict, f, ensure_ascii=False, indent=4)
 
 
@@ -96,7 +167,7 @@ def get_current_value(quality):
     params.update(page=wikidict[quality]["name/history"])
     wiki_req = requests.get(url=api_endpoint, params=params, headers=headers)
     #TODO: catch bad requests
-    wiki_txt = wiki_req.content.decode()
+    wiki_txt = wiki_req.json()["parse"]["wikitext"]["*"]
     value_update = parse_current_value(wiki_txt)
 
     return value_update
@@ -113,61 +184,8 @@ def get_latest_values(qualities):
     return values_dict, min_update
 
 
-def format_wikiurl(string: str):
-    rgx_simple = r"\[\[([ \-\w]+)\]\]"
-    rgx_alias = r"\[\[([ \-\w]+)\|([ \-\w]+)\]\]"
-    simple_links = finditer(rgx_simple, string)
-    for match in simple_links:
-        url = wiki_url + match.group(1)
-        href = f"<a href=\"{url}\">{match.group(1)}</a>"
-        string = string.replace(match.group(0), href)
-
-    alias_links = finditer(rgx_alias, string)
-    for match in alias_links:
-        url = wiki_url + match.group(1)
-        href = f"<a href=\"{url}\">{match.group(2)}</a>"
-        string = string.replace(match.group(0), href)
-
-    return string
-
-def format_npfwikiurl(block: dict):
-    rgx_simple = r"\[\[([ \-\w']+)\]\]"
-    rgx_alias = r"\[\[([ \-\w']+)\|([ \-\w']+)\]\]"
-
-    formatting = []
-    simple_links = finditer(rgx_simple, string)
-
-    for match in simple_links:
-        url = wiki_url + match.group(1).replace(" ", "_")
-        start = string.find(match.group(0))
-        end = start + len(match.group(1))
-        formatting.append({
-            "start": start,
-            "end": end,
-            "type": "link",
-            "url": url
-        })
-        print(formatting[-1])
-
-        string = string.replace(match.group(0), match.group(1))
-
-    alias_links = finditer(rgx_alias, string)
-    for match in alias_links:
-        url = wiki_url + match.group(1).replace(" ", "_")
-        start = string.find(match.group(0))
-        end = start + len(match.group(2))
-        formatting.append({
-            "start": start,
-            "end": end,
-            "type": "link",
-            "url": url
-        })
-        print(formatting[-1])
-        string = string.replace(match.group(0), match.group(2))
-
-    block["formatting"] = formatting
-
 
 if __name__ == "__main__":
     parse_quality_values()
     dump_items()
+    parse_all_stats()
