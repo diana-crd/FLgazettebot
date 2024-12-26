f3ed359161371ab4adbfc7c23f557698fb71a13b
Diana-CRD <cervidiana327@gmail.com>
2024-12-26 14:52:46 +0100

    moved post testing to separate file

--- update.py
+++ update.py
@@ -37,52 +37,6 @@ def tumblr_login():
 
     return client
 
-def test_post(day, quality=None, draft_post=True):
-
-    if not wq_templates[day]:
-        return None
-    
-    template_path = f"content/{wq_templates[day]}.json"
-    post_template = posts.open_post(template_path)
-
-    if quality:
-        qualities = [quality]
-    
-    else:
-        to_check = post_template["qualities"]["to_check"]
-        other_qualities = post_template["qualities"]["other"]
-        qualities = to_check + other_qualities
-
-    client = tumblr_login()
-    print(f"{day}:")
-    for quality in qualities:
-        print(f"\t{quality}")
-        content = [{
-            "type": "text",
-            "subtype": "heading1",
-            "text": f"{day} {quality} test"
-        }]
-        content.extend(posts.test_quality(post_template[quality], quality))
-
-        tags = ["test", day, quality]
-        posts.format_npfall(content)
-        if draft_post:
-            result = client.create_post(blogname, content=content, tags=tags, state="draft")
-            if "errors" in result.keys():
-                for block in content[1:]:
-                    sngl_result = client.create_post(blogname, content=[block], tags=tags, state="draft")
-                    print(sngl_result)
-        if not draft_post:
-            for block in content:
-                print(block)
-                print(f"Text length: {len(block['text'])}")
-        
-
-
-def test_posts():
-    for day in wq_templates.keys():
-        test_post(day)
-
 
 # not implemented: check for presence of live events
 def live_event_check():
@@ -152,19 +106,4 @@ def wq_update():
 
 if __name__ == "__main__":
 
-    if len(sys.argv) > 1:
-        task = sys.argv[1]
-    else:
-        task = "update"
-
-    if task == "update":
-        print(wq_update())
-    elif task == "test":
-        if len(sys.argv) == 3:
-            test_post(sys.argv[2])
-        elif len(sys.argv) == 4:
-            test_post(sys.argv[2], sys.argv[3])
-        else:
-            test_posts()
-
-    
+    print(wq_update())
